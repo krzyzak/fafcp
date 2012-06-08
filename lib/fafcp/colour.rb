6a15171822b6ef13cbd078d2afd27d2b03a99ce9
Michał Krzyżanowski <michal.krzyzanowski+github@gmail.com>
2012-06-08 09:54:06 +0200

    first version

--- lib/fafcp/colour.rb
+++ lib/fafcp/colour.rb
@@ -1,4 +1,31 @@
 module Fafcp
   class Colour
+    def initialize(colour)
+      @colour = colour
+    end
+
+    def self.from_hex(hex)
+      new(hex[1, 6])
+    end
+
+    def self.from_int(num)
+      new(num.to_i(16).to_s)
+    end
+
+    def to_s
+      "##{@colour.to_s}"
+    end
+
+    def red
+      @colour[0, 2].to_i(16)
+    end
+
+    def green
+      @colour[2, 2].to_i(16)
+    end
+
+    def blue
+      @colour[4, 2].to_i(16)
+    end
   end
 end
--- lib/fafcp/picker.rb
+++ lib/fafcp/picker.rb
@@ -1,4 +1,31 @@
 module Fafcp
   class Picker
+    attr_writer :colours
+    def initialize(number_of_colours_to_pick, colours = {})
+      @number_of_colours_to_pick = number_of_colours_to_pick
+      @colours = colours#.map{|colour| Colour.from_hex(colour)}
+    end
+
+    def pick
+      palette = {}
+      @colours.combination(@number_of_colours_to_pick).each do |colours|
+        diff = 0
+        colours.combination(2).each do |first, second|
+          diff += difference(first, second)
+        end
+        palette.merge!({diff => colours})
+      end
+      palette.max_by{|key, value| key.to_s.to_i}[1]
+    end
+
+    def difference(first, second)
+      mean_r = (first.red - second.red) / 2
+      delta_r = first.red - second.red
+      delta_g = first.green - second.green
+      delta_b = first.blue - second.blue
+
+      diff = (2 + mean_r/256) * delta_r**2 + 4 * delta_g**2 + (2 + (255 - mean_r)/256) * delta_b**2
+      Math.sqrt(diff)
+    end
   end
 end
